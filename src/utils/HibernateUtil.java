8dcf253f3a852b18a82e9aa61849f4eaa8024c92
zzyviolette <31993422+zzyviolette@users.noreply.github.com>
2018-11-15 23:12:46 +0100

    Update HibernateUtil.java

--- src/utils/HibernateUtil.java
+++ src/utils/HibernateUtil.java
@@ -1,8 +1,16 @@
 package utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
  
 public class HibernateUtil {
  
@@ -10,7 +18,19 @@ public class HibernateUtil {
  
     static {
         try {
-            sessionFactory = new AnnotationConfiguration().configure("hibernate.cfg.xml").buildSessionFactory();
+        	
+            Map<String,String> jdbcUrlSettings = new HashMap<>();
+            String jdbcDbUrl = System.getenv("JDBC_DATABASE_URL");
+            if (null != jdbcDbUrl) {
+              jdbcUrlSettings.put("hibernate.connection.url", System.getenv("JDBC_DATABASE_URL"));
+            }
+
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder().
+                configure("hibernate.cfg.xml").
+                applySettings(jdbcUrlSettings).
+                build();
+//            sessionFactory = new AnnotationConfiguration().configure("hibernate.cfg.xml").buildSessionFactory();
+            sessionFactory = new Configuration().buildSessionFactory(registry);
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
