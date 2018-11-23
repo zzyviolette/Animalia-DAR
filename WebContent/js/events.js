5fff35670ebecf5ab7ca79f259490f08a014f83d
zzyviolette <31993422+zzyviolette@users.noreply.github.com>
2018-11-23 01:16:59 +0100

    Update events.js

--- WebContent/js/events.js
+++ WebContent/js/events.js
@@ -14,7 +14,7 @@ $.ajax({
 		"url" : "event",
 		"type" : "post",
 		"data" : {
-		  "action" : 'display_mine'
+		  "action" : 'display_all'
 		},
 		"dataType" : "json",
 		"success" : function( events, textStatus, jqXHR) {
