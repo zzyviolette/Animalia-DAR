acac221e00f4192799577ca7a26cecdf8fc89311
zzyviolette <31993422+zzyviolette@users.noreply.github.com>
2019-01-15 21:09:21 +0100

    Update AccessoiresServlet.java

--- src/controller/AccessoiresServlet.java
+++ src/controller/AccessoiresServlet.java
@@ -26,51 +26,12 @@ public class AccessoiresServlet extends HttpServlet {
 	public void searchItem(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("application/json;charset=utf-8");
 	
-//		List<SearchItem> searchItems = findItem(req.getParameter("obj"));
-		
-//		JSONArray jsonArray = new JSONArray();
-//		if (searchItems != null) {
-//			for (SearchItem item : searchItems) {
-//				JSONObject jsonObject = new JSONObject();
-//				jsonObject.put("titre", item.getTitle());
-//				jsonObject.put("photo", item.getGalleryURL());
-//				jsonObject.put("info", item.getViewItemURL());
-//				jsonObject.put("price", "US $"+item.getSellingStatus().getConvertedCurrentPrice().getValue());
-//				jsonArray.put(jsonObject);
-//			}
-//
-//		}
+
 		EbayService ebayService = new EbayService();
 		String jsonStr = ebayService.findItem(req.getParameter("obj"));
 		PrintWriter out = resp.getWriter();
 		out.println(jsonStr);
 	}
 
-//	public List<SearchItem> findItem(String obj) {
-//		List<SearchItem> items = null;
-//		try {
-//			// initialize service end-point configuration
-//			ClientConfig config = new ClientConfig();
-//			// endpoint address can be overwritten here, by default, production
-//			// address is used,
-//			// to enable sandbox endpoint, just uncomment the following line
-//			// config.setEndPointAddress("http://svcs.sandbox.ebay.com/services/search/FindingService/v1");
-//			config.setApplicationId("ZiyiZhou-Animal-PRD-9f9002d37-0e2d0833");
-//			// create a service client
-//			FindingServicePortType serviceClient = FindingServiceClientFactory.getServiceClient(config);
-//			// create request object
-//			FindItemsByKeywordsRequest request = new FindItemsByKeywordsRequest();
-//			// set request parameters
-//			request.setKeywords(obj);
-//			// call service
-//			FindItemsByKeywordsResponse result = serviceClient.findItemsByKeywords(request);
-//			// output result
-//			items = result.getSearchResult().getItem();
-//
-//		} catch (Exception ex) {
-//			// handle exception if any
-//			ex.printStackTrace();
-//		}
-//		return items;
-//	}
+
 }
